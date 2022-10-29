d6318ffc0652ac9bd53ffcd9368227536bcc7449
henry-thiongi <boblilhot@gmail.com>
2022-10-30 00:33:56 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter {|w| w.chars.sort == @word.chars.sort}
+    end
+end 
